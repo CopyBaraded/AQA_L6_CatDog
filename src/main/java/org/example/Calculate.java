a2d383eae049671de18175f082e98a971fb465e5
Baraded <borodeda@gmail.com>
2025-03-03 21:53:17 +0300

    interface test

---
+++ src/main/java/org/example/Calculate.java
@@ -0,0 +1,23 @@
+package org.example;
+/*
+Применяя интерфейсы написать программу расчета периметра и площади геометрических фигур: круг, прямоугольник, треугольник.
+Задать для каждой фигуры цвет заливки и цвет границы.
+Результат полученных характеристик [ Периметр, площадь, цвет фона, цвет границ ] по каждой фигуре вывести в консоль.
+Попробуйте реализовать базовые методы, такие как расчет периметра фигур, в качестве дефолтных методов в интерфейсе.
+ */
+
+public interface Calculate {
+    double getArea();
+    double getPerimeter();
+    String getColorBody();
+    String getColorBorder();
+
+    default void printInfo () {
+        System.out.println("Area: " + getArea()
+                + "\nPer: " + getPerimeter()
+                + "\n color body:" + getColorBody()
+                + "\n color bord: " + getColorBorder());
+
+
+    }
+}
